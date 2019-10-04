24ee798afc963f9da2f96fbd178450544b4f24ba
fernandabelenVL <fernanda.vegaleiva@gmail.com>
2019-10-04 18:12:36 -0300

    Updates

---
+++ precache-manifest.12a78085bca23ea0c5d433af426577b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5f88496cfbe0a6104aad0b412fedaa",
+    "url": "/SCL010-Burger-Queen-v2/index.html"
+  },
+  {
+    "revision": "2b06adddd5bb7a62c681",
+    "url": "/SCL010-Burger-Queen-v2/static/css/2.4e0883bb.chunk.css"
+  },
+  {
+    "revision": "73c7ccd7405c05b73bc4",
+    "url": "/SCL010-Burger-Queen-v2/static/css/main.25c7fd17.chunk.css"
+  },
+  {
+    "revision": "2b06adddd5bb7a62c681",
+    "url": "/SCL010-Burger-Queen-v2/static/js/2.71aca697.chunk.js"
+  },
+  {
+    "revision": "73c7ccd7405c05b73bc4",
+    "url": "/SCL010-Burger-Queen-v2/static/js/main.23475077.chunk.js"
+  },
+  {
+    "revision": "1def0f09b4b8400a07f8",
+    "url": "/SCL010-Burger-Queen-v2/static/js/runtime-main.fc4421b8.js"
+  },
+  {
+    "revision": "8a300f700382461962dc91e01a63c8cf",
+    "url": "/SCL010-Burger-Queen-v2/static/media/bg-image@2x.8a300f70.png"
+  },
+  {
+    "revision": "5d9005735e8efd7059e9e949947b351d",
+    "url": "/SCL010-Burger-Queen-v2/static/media/big-burger@2x.5d900573.png"
+  }
+]);
